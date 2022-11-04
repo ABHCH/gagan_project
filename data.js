973deeb85bedfa4459c4fdc79002fa935ce94d7d
Abhishek Chaturvedi <65055220+ABHCH@users.noreply.github.com>
2022-11-04 21:33:11 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id:1,
+        cardImage:'https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        foodName:'Veggies Pasta',
+        authorName:'By Jane Doe',
+        recpies:'A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.'
+    },
+    {
+        id:2,
+        cardImage:'https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        foodName:'Veggies Pasta',
+        authorName:'By Jane Doe',
+        recpies:'A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.'
+    },
+    {
+        id:3,
+        cardImage:'https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        foodName:'Veggies Pasta',
+        authorName:'By Jane Doe',
+        recpies:'A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.'
+    },
+    {
+        id:4,
+        cardImage:'https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        foodName:'Veggies Pasta',
+        authorName:'By Jane Doe',
+        recpies:'A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.'
+    },
+    {
+        id:5,
+        cardImage:'https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        foodName:'Veggies Pasta',
+        authorName:'By Jane Doe',
+        recpies:'A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.'
+    },
+    {
+        id:6,
+        cardImage:'https://images.unsplash.com/photo-1624726175512-19b9baf9fbd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        foodName:'Veggies Pasta',
+        authorName:'By Jane Doe',
+        recpies:'A handful of simple ingredients typify the fresh, vibrant flavors of Greek cooking.'
+    },
+]
